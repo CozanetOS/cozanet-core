f69609eccfd99fb81263b0c2e8557e6040f3f37b
CozyCrypto <fassdavid722@gmail.com>
2026-07-10 23:18:05 +0100

    feat: create barrel export

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { CEO } from "./CEO/ceo";
+export * from "./CEO/types";
+export { IdentityEngine, UserIdentity, Personality } from "./Identity/identity";
+export { PlanningEngine, Plan, PlanStep, PlanResult } from "./Planning/planning";
+export { ReasoningEngine, ReasoningResult } from "./Reasoning/reasoning";
+export { DecisionEngine, DecisionResult } from "./Decision/decision";
+export { boot } from "./Bootloader/bootloader";
